a7acf5c67636c552b133bb0693276fa04fe872da
JesseLudtke <jludtke@sfu.ca>
2017-02-26 18:42:38 -0800

    Initial Commit
    Includes a basic script for symlinking dotfiles and a simple vimrc.

---
+++ symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir=~/dotfiles
+backupdir=~/dotfiles_backup
+files="vimrc"
+
+mkdir -p $backupdir
+
+for file in $files; do
+    echo "Moving .$file from ~ to $backupdir."
+    mv ~/.$file $backupdir
+    echo "Creating symlink to $file in ~."
+    ln -s $dir/$file ~/.$file
+done
+
